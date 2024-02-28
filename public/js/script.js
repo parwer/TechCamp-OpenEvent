1edc3fd7da1be120132d6e3348e0364ac8785c19
parwer <55444278+parwer@users.noreply.github.com>
2024-02-28 20:32:36 +0700

    init

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 var socket = io();
 var mainVideo = document.getElementById("main-video");
 var secondaryVideo = document.getElementById("secondary-video");
+var footageVideo = document.getElementById("footage-video");
 
 socket.on('index', (check) => {
   // stop video
@@ -9,7 +10,26 @@ socket.on('index', (check) => {
     secondaryVideo.style.display = "block";
     secondaryVideo.play();
     secondaryVideo.addEventListener("ended", function () {
+      footageVideo.style.display = "block";
+      footageVideo.play();
+
       secondaryVideo.style.display = "none";
     });
   }
 });
+
+const chkbutton = () => {
+    secondaryVideo.style.display = "block";
+    secondaryVideo.play();
+    secondaryVideo.addEventListener("ended", () => {
+        footageVideo.style.display = "block";
+        footageVideo.play();
+        secondaryVideo.style.display = "none";
+    });
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode == 13) {
+    chkbutton();
+  }
+}, true);
