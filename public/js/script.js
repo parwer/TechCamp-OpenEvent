57349e7257e22518d40c0d67bf8eb87ac14203cf
parwer <parwer.ukha@gmail.com>
2024-02-29 00:27:50 +0700

    AHHHHHHHHHHHHHHHH

--- public/js/script.js
+++ public/js/script.js
@@ -3,27 +3,29 @@ var mainVideo = document.getElementById("main-video");
 var secondaryVideo = document.getElementById("secondary-video");
 var footageVideo = document.getElementById("footage-video");
 
+function playSecondaryVideo() {
+  secondaryVideo.style.display = "block";
+  secondaryVideo.play();
+  secondaryVideo.addEventListener("ended", function () {
+    secondaryVideo.style.display = "none";
+  });
+}
+
 socket.on('index', (check) => {
   // stop video
   secondaryVideo
   if (check) {
-    secondaryVideo.style.display = "block";
-    secondaryVideo.play();
-    secondaryVideo.addEventListener("ended", function () {
-      footageVideo.style.display = "block";
-      footageVideo.play();
-
-      secondaryVideo.style.display = "none";
-    });
+    playSecondaryVideo();
   }
 });
+
 
 const chkbutton = () => {
     secondaryVideo.style.display = "block";
     secondaryVideo.play();
     secondaryVideo.addEventListener("ended", () => {
-        footageVideo.style.display = "block";
-        footageVideo.play();
+        // footageVideo.style.display = "block";
+        // footageVideo.play();
         secondaryVideo.style.display = "none";
     });
 }
